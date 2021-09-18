381120705905d13b0d43dae30211aecbcb60f033
kimxwan <kimxwan0319@naver.com>
2021-09-18 11:44:08 +0900

    ⚡️ :: ASTextNode의 String만 바꾸는 Extension 함수 개발

---
+++ Sources/Extension/ASTextNode.swift
@@ -0,0 +1,18 @@
+//
+//  ASTextNode.swift
+//  iDelivery
+//
+//  Created by 김수완 on 2021/09/17.
+//  Copyright © 2021 com.kimxwan0319. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+extension ASTextNode {
+    public func setString(_ string: String) {
+        self.attributedText = NSAttributedString(
+            string: string,
+            attributes: self.attributedText?.attributes(at: 0, effectiveRange: nil)
+        )
+    }
+}
