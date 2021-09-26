6b2231227d65a2a4bfa4903ba0edf5731d6fa003
kimxwan <kimxwan0319@naver.com>
2021-09-26 17:02:50 +0900

    ⚡️ :: DeliveryCompany 모델

---
+++ Sources/Domain/Model/DeliveryCompany.swift
@@ -0,0 +1,14 @@
+//
+//  DeliveryCompany.swift
+//  iDelivery
+//
+//  Created by 김수완 on 2021/09/26.
+//  Copyright © 2021 com.kimxwan0319. All rights reserved.
+//
+
+import Foundation
+
+struct DeliveryCompany {
+    let id : String
+    let name : String
+}
