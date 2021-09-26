3f4d68bbe541a1cf542d9510230265b271d1943e
kimxwan <kimxwan0319@naver.com>
2021-09-26 17:03:52 +0900

    ⚡️ :: DeliveryCompanyDTO

---
+++ Sources/Data/Model/DeliveryCompanyListDTO.swift
@@ -0,0 +1,26 @@
+//
+//  DeliveryCompanyListDTO.swift
+//  iDelivery
+//
+//  Created by 김수완 on 2021/09/26.
+//  Copyright © 2021 com.kimxwan0319. All rights reserved.
+//
+
+import Foundation
+
+// MARK: - Data Transfer Object
+struct DeliveryCompanyDTO: Decodable {
+    let id : String
+    let name : String
+    let tel: String
+}
+
+// MARK: - Mappings to Domain
+extension DeliveryCompanyDTO {
+    func toDomain() -> DeliveryCompany {
+        return .init(
+            id: id,
+            name: name
+        )
+    }
+}
