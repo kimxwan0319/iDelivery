b2284c0934daefb64d0ba1b690fad0811c52640d
kimxwan <kimxwan0319@naver.com>
2021-09-30 23:45:40 +0900

    ⚡️ :: DeleteParcelUseCase

---
+++ Sources/Domain/UseCase/DeleteParcelUseCase.swift
@@ -0,0 +1,23 @@
+//
+//  DeleteParcelUseCase.swift
+//  iDelivery
+//
+//  Created by 김수완 on 2021/09/30.
+//  Copyright © 2021 com.kimxwan0319. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+
+class DeleteParcelUseCase {
+
+    private let userParcelsRepository : UserParcelsRepository
+
+    init(userParcelsRepository: UserParcelsRepository) {
+        self.userParcelsRepository = userParcelsRepository
+    }
+
+    func execute(userParcel: UserParcel) {
+        return userParcelsRepository.deleteUserParcel(parcel: userParcel)
+    }
+}
