245ccc9df523a04eb7f56191327eda9b216a5157
kimxwan <kimxwan0319@naver.com>
2021-10-03 17:30:17 +0900

    ♻️ :: 네이밍 변경 `UserParcel` -> `Parcel`

--- Sources/Domain/UseCase/SaveParcelUseCase.swift
+++ Sources/Domain/UseCase/SaveParcelUseCase.swift
@@ -13,7 +13,7 @@ class SaveParcelUseCase {
 
     @Inject private var userParcelsRepository : UserParcelsRepository
 
-    func execute(userParcel: UserParcel) {
+    func execute(userParcel: Parcel) {
         return userParcelsRepository.saveUserParcel(parcel: userParcel)
     }
 }
