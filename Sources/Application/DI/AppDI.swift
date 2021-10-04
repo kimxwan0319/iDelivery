78472402f5ea8c4d936f2cfbe61aac862c4b1e98
kimxwan <kimxwan0319@naver.com>
2021-10-04 21:20:12 +0900

    ⚡️ :: DI추가 및 메소드의 접근제어지시자 변경

--- Sources/Application/DI/AppDI.swift
+++ Sources/Application/DI/AppDI.swift
@@ -9,21 +9,21 @@
 import Swinject
 
 extension Container {
-    func registerDependencies() {
+    public func registerDependencies() {
         registerRepositories()
         registerUseCases()
         registerReactors()
         registerNodeControllers()
     }
 
-    fileprivate func registerRepositories() {
+    private func registerRepositories() {
         self.register(UserParcelsRepository.self) { _ in DefaultUserParcelsRepository() }
         self.register(DeliveryCompaniesRepository.self) { _ in DefaultDeliveryCompaniesRepository() }
         self.register(ParcelInformationRepository.self) { _ in DefaultParcelInformationRepository() }
     }
 
-    fileprivate func registerUseCases() {
-        self.register(FetchParcelListUseCase.self) { _ in FetchParcelListUseCase() }
+    private func registerUseCases() {
+        self.register(FetchDeliveryCompaniesUseCase.self) { _ in FetchDeliveryCompaniesUseCase() }
         self.register(SaveParcelUseCase.self) { _ in SaveParcelUseCase() }
         self.register(DeleteParcelUseCase.self) { _ in DeleteParcelUseCase() }
         self.register(FetchParcelListUseCase.self) { _ in FetchParcelListUseCase() }
@@ -31,11 +31,20 @@ extension Container {
         self.register(CheckParcelStateUseCase.self) { _ in CheckParcelStateUseCase() }
     }
 
-    fileprivate func registerReactors() {
+    private func registerReactors() {
+        self.register(ParcelListReactor.self) { _ in ParcelListReactor() }
     }
 
-    fileprivate func registerNodeControllers() {
-        self.register(ParcelListNodeController.self) { _ in ParcelListNodeController() }
-        self.register(ParcelDetailNodeController.self) { _ in ParcelDetailNodeController() }
+    private func registerNodeControllers() {
+        self.register(ParcelListNodeController.self) { resolver in
+            let controller = ParcelListNodeController()
+            controller.reactor = resolver.resolve(ParcelListReactor.self)
+            return controller
+        }
+        self.register(ParcelDetailNodeController.self) { resolver in
+            let controller = ParcelDetailNodeController()
+            // controller.reactor = resolver.resolve(ParcelDetailReactor.self)
+            return controller
+        }
     }
 }
