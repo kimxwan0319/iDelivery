b5df071c9e2238a59b9101d6cfdde67d8075a51d
kimxwan <kimxwan0319@naver.com>
2021-10-17 22:12:10 +0900

    ⚡️ :: ParcelDetailReactor 틀

--- Sources/Presentataion/Scene/ParcelDetail/ParcelDetailReactor.swift
+++ Sources/Presentataion/Scene/ParcelDetail/ParcelDetailReactor.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2021 com.kimxwan0319. All rights reserved.
 //
 
-import Foundation
+import ReactorKit
+import RxSwift
+import RxCocoa
+
+class ParcelDetailReactor: Reactor {
+
+    private let disposeBag = DisposeBag()
+    let initialState: State
+    
+    // MARK: Action
+    enum Action {
+    }
+
+    // MARK: Mutation
+    enum Mutation {
+    }
+    
+    // MARK: State
+    enum State {
+    }
+}
+
+// MARK: - Action -> Mutation
+extension ParcelDetailReactor {
+    func mutate(action: Action) -> Observable<Mutation> {
+    }
+}
+
+// MARK: - Mutation -> State
+extension ParcelDetailReactor {
+    func reduce(state: State, mutation: Mutation) -> State {
+        var newState = state
+        return newState
+    }
+}
