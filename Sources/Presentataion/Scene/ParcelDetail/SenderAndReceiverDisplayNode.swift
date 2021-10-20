481fced3df84f5c0ad23cb55165b5f7bb20989fc
kimxwan <kimxwan0319@naver.com>
2021-10-20 22:49:26 +0900

    📑 :: SenderAndReceiverDisplayNode

---
+++ Sources/Presentataion/Scene/ParcelDetail/SenderAndReceiverDisplayNode.swift
@@ -0,0 +1,14 @@
+//
+//  SenderAndReceiverDisplayNode.swift
+//  iDelivery
+//
+//  Created by 김수완 on 2021/10/20.
+//  Copyright © 2021 com.kimxwan0319. All rights reserved.
+//
+
+import Foundation
+import AsyncDisplayKit
+
+class SenderAndReceiverDisplayNode: ASDisplayNode {
+    
+}
