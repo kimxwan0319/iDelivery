48ff918c9f3ca48e2ca1ae34dffdedffd7a4bd0d
kimxwan <kimxwan0319@naver.com>
2021-10-22 19:27:59 +0900

    ⚡️ :: ASLineNode

---
+++ Sources/Presentataion/Extension+Subclass/ASLineNode.swift
@@ -0,0 +1,21 @@
+//
+//  ASLineNode.swift
+//  iDelivery
+//
+//  Created by 김수완 on 2021/10/22.
+//  Copyright © 2021 com.kimxwan0319. All rights reserved.
+//
+
+import AsyncDisplayKit
+
+class ASLineNode: ASDisplayNode {
+    enum Direction { case vertical; case horizontal }
+    init(direction: Direction, color: UIColor, width: CGFloat) {
+        super.init()
+        self.backgroundColor = color
+        switch direction {
+        case .vertical: self.style.preferredSize.width = 1
+        case .horizontal: self.style.preferredSize.height = 1
+        }
+    }
+}
